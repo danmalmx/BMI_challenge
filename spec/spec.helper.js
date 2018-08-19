8c49334723ef7f0f2c1a411a841fec8e75bf4c58
danmalmx <danmalmx@gmail.com>
2018-08-19 21:04:46 +0200

    some blockers still

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -2,5 +2,7 @@ const fs = require('fs');
 const chai = require('chai');
 global.expect = chai.expect;
 
+let calc = fs.readFileSync('./src/js/calculator.js');
 let person = fs.readFileSync('./src/js/person.js');
-eval( person + `\nexports.Person = Person;`)
+eval( person + `\nexports.Person = Person;`);
+eval( calc + `\nexports.BMICalculator = BMICalculator;`)
