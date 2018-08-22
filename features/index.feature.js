c71596849a97da70dc125cf806d47422e38b2cd0
danmalmx <danmalmx@gmail.com>
2018-08-22 18:59:24 +0200

    changes, incl imperial, still mistakew

--- features/index.feature.js
+++ features/index.feature.js
@@ -44,11 +44,11 @@ describe("User can calculate BMI using the imperial method", () => {
 
 
     it("by inputing his weight and height", async () => {
-        await browser.selectOption("select[id='my_select']", {option: 'imperial'})
-        await browser.fillIn("input[id='weight-in-lbs']", {with: "170"})
-        await browser.fillIn("input[id='height-in-in']", {with: "70"}) 
+        await browser.selectRadioButton("select[id='my_select']", {option: 'imperial'});
+        await browser.fillIn("input[id='weight-in-lbs']", {with: "170"});
+        await browser.fillIn("input[id='h   eight-in-in']", {with: "70"}); 
 
-        await browser.clickOnButton("button")
+        await browser.clickOnButton("button");
         let content = await browser.getContent("span[id='display_value']");
         expect(content).to.eql("Your BMI is 24.39")
     });
